db35de1483cfbc7a8f7a440a8ab423f2d177fff3
Monica Tamaru Jones <mntamaru@gmail.com>
2019-03-21 21:04:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,5 @@ const app = "I don't do much.";
 
 const digitalClock = {
   time: Math.round(Date.now() / 1000), 
-  startTicking() {
-    setInterval(this.time++, 1000);
-  }
-  
+  startTicking: function() { setInterval(() => this.time++, 1000); }
 };
